a0da3270b915d3c1327ae4e3b96b221c8bb3edc0
namubar <mubarakshin.nail@tatar.ru>
2022-11-07 21:14:52 +0300

    initial commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿int[] myArray = new int[8];
+void ArrayPrint (int [ ] myArray)
+{
+    for (int i = 0; i < myArray.Length; i++)
+    {
+        Console.WriteLine($"Введите {i} элемент массива: \t");
+        myArray[i] = int.Parse(Console.ReadLine()!);
+    }
+    for (int i = 0; i < myArray.Length; i++)
+    {
+        Console.Write("\t " + myArray[i] );
+    }
+}
+ArrayPrint (myArray);
